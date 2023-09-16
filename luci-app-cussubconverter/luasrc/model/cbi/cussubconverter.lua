6132e2bc31e2197e7383b9384354997ff83bd340
lhn1989 <lhn1989@users.noreply.github.com>
2023-09-16 21:17:24 +0800

    Update cussubconverter.lua

--- luci-app-cussubconverter/luasrc/model/cbi/cussubconverter.lua
+++ luci-app-cussubconverter/luasrc/model/cbi/cussubconverter.lua
@@ -7,7 +7,7 @@ s.addremove = false
 s.anonymous = true
 
 conurl = s:option(Value, "conurl", translate("Config URL"))
-conurl.placeholder = 'https://raw.githubusercontent.com/lhn1989/Rules/master/Pro
+conurl.placeholder = 'https://raw.githubusercontent.com/lhn1989/Rules/master/Subscription/config.yaml'
 suburl = s:option(DynamicList, "suburl", translate("Subscription URL"))
 
 return m
