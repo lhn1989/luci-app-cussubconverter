682ad530bbc57528d658782eb14eb75e23ee9c04
lhn1989 <lhn1989@users.noreply.github.com>
2023-09-16 21:53:03 +0800

    Update cussubconverter.lua

--- luci-app-cussubconverter/luasrc/controller/cussubconverter.lua
+++ luci-app-cussubconverter/luasrc/controller/cussubconverter.lua
@@ -1,7 +1,7 @@
 module("luci.controller.cussubconverter", package.seeall)
 
 function index()
-        entry({"admin", "services", "cussubconverter"}, cbi("cussubconverter"), _("CusSubConverter"), 10)
+        entry({"admin", "services", "cussubconverter"}, cbi("cussubconverter"), _("CusSubConverter"), 100)
         entry({"admin", "services", "cussubconverter", "convert"}, call("convert"))
 end
 
