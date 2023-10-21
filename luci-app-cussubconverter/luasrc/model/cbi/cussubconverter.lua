6dd51bce57e6a93ca39b867b0fac90f320e9b9ff
lhn1989 <lhn1989@users.noreply.github.com>
2023-10-21 11:50:23 +0800

    Update cussubconverter.lua

--- luci-app-cussubconverter/luasrc/model/cbi/cussubconverter.lua
+++ luci-app-cussubconverter/luasrc/model/cbi/cussubconverter.lua
@@ -1,13 +1,13 @@
 require("luci.sys")
 
-font_red = [[<b style=color:green>]]
+font_green = [[<b style=color:green>]]
 font_off = [[</b>]]
 
 m = Map("cussubconverter", translate("Custom Subscription Converter"))
 m.description = translate("A Custom Subsription Converter For Ryan and his friends.")..
 "<br/>"..
 "<br/>"..translate("Convertered URL: ")..
-"<br/>"..font_red.."http://192.168.100.1/cgi-bin/cussubconverter"..font_off
+"<br/>"..font_green.."http://192.168.100.1/cgi-bin/cussubconverter"..font_off
 
 s = m:section(TypedSection, "cussubconverter", "")
 s.addremove = false
